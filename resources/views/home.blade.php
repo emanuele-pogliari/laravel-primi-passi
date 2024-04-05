8037c2bf63720ab0c514a16a3c3a381e25775bcf
emanuele-pogliari <manu.pogliari@gmail.com>
2024-04-05 15:27:18 +0200

    pages created and connected

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
     <title>Document</title>
 </head>
 
@@ -12,6 +14,7 @@
     {{$message}}
 
     <h4>{{$boolean}}</h4>
+
     <ul>
         @foreach($array as $value)
         <li>
@@ -19,6 +22,10 @@
         </li>
         @endforeach
     </ul>
+    <a href="{{route('page2')}}">pagina2</a>
+    <a href="{{route('page3')}}">pagina3</a>
+    <a href="{{route('page4')}}">pagina4</a>
+    <a href="{{route('page5')}}">pagina5</a>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,23 @@ Route::get('/', function () {
 
     return view('home', compact("message", "boolean", "array"));
 });
+
+Route::get('/page2', function () {
+    $msgPage2 = "Hello World Page2!";
+    return view('page2', compact("msgPage2"));
+})->name('page2');
+
+Route::get('/page3', function () {
+    $msgPage3 = "Hello World Page3!";
+    return view('page3', compact("msgPage3"));
+})->name('page3');
+
+Route::get('/page4', function () {
+    $msgPage4 = "Hello World Page4!";
+    return view('page4', compact("msgPage4"));
+})->name('page4');
+
+Route::get('/page5', function () {
+    $msgPage5 = "Hello World Page5!";
+    return view('page5', compact("msgPage5"));
+})->name('page5');
